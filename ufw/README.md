3698253395c743cd119e340ae03fbff62ad55499
bogeholm <troelsmikkelsen@gmail.com>
2021-02-04 20:20:04 +0100

    Add UFW files

---
+++ ufw/README.md
@@ -0,0 +1,9 @@
+# `ufw` setup
+
+## Links
+
+- https://www.digitalocean.com/community/tutorials/how-to-setup-a-firewall-with-ufw-on-an-ubuntu-and-debian-cloud-server
+- https://linuxize.com/post/how-to-setup-a-firewall-with-ufw-on-ubuntu-18-04/
+- https://www.digitalocean.com/community/tutorials/ufw-essentials-common-firewall-rules-and-commands
+- https://www.raspberrypi.org/documentation/configuration/security.md
+
---
+++ ufw/setup-ufw.sh
@@ -0,0 +1,20 @@
+#! /use/bin/env bash
+
+set -x
+
+# Defaults
+ufw default deny incoming
+ufw default allow outgoing
+
+# SSH
+ufw allow proto tcp from 192.168.3.0/24 to any port 22
+ufw allow proto tcp from 192.168.9.0/24 to any port 22
+
+# HTTP / HTTPS
+ufw allow proto tcp from 192.168.3.0/24 to any port 80,443
+ufw allow proto tcp from 192.168.9.0/24 to any port 80,443
+
+# DNS - equivalent to
+# sudo ufw allow 53/tcp
+# sudo ufw allow 53/udp
+sudo ufw allow dns

df251d132ccfcbeb3f9d06b0f56556231f62b7a3
bogeholm <troelsmikkelsen@gmail.com>
2021-02-08 23:36:29 +0100

    Add script for getting latest GitHub release

---
+++ cloudflared/github-latest.sh
@@ -0,0 +1,17 @@
+github_latest () {
+    ORG_REPO=${1:-"cloudflare/cloudflared"}
+    # https://docs.github.com/en/rest/reference/repos#releases
+    URL="https://api.github.com/repos/${ORG_REPO}/releases/latest"
+    HEADER="Accept: application/vnd.github.v3+json"
+
+    # TODO: Capture this properly
+    which_jq="$(which jq)"
+
+    if [[ $? != 0 ]]; then
+        echo "jq not found" >&2
+        return 1
+    fi
+
+    latest=$(curl -sSfL --proto '=https' --tlsv1.2 -H "${HEADER}" "${URL}" | jq .tag_name)
+    echo "${latest}"
+}
